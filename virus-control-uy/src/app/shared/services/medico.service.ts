acbe4f219a2815d4696b9745f09b753f51cc28a4
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-06-05 20:01:54 -0300

    agrego token e interceptor

--- virus-control-uy/src/app/shared/services/medico.service.ts
+++ virus-control-uy/src/app/shared/services/medico.service.ts
@@ -15,12 +15,16 @@ export class MedicoService {
     console.log('this.autenticacionService.user.email', this.autenticacionService.user);
 
 
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // headers.append('Authorization', 'Bearer ');
+    // let options = new RequestOptions({ headers: headers });
 
     return this.http.get(`${environment.url_backend}/medico/${this.autenticacionService.user.username}/visita_pendiente/all`);
   }
 
 
   public getExamenes(){
+    
     return this.http.get(`${this.baseUrl}/examenesenfermedad/100`);
   }
 
