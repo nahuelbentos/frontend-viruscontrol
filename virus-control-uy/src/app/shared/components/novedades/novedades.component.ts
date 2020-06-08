ce094a51db87d462867c3b5c6d7a306e34ab53c5
Santiago Chabert <santiagochabert@gmail.com>
2020-06-08 02:19:23 -0300

    ES EL TWITTER PAPAAAA

--- virus-control-uy/src/app/shared/components/novedades/novedades.component.ts
+++ virus-control-uy/src/app/shared/components/novedades/novedades.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Novedad } from '@shared/model/Novedad';
 import { PublicService } from '@shared/services/public.service';
@@ -16,9 +16,13 @@ export class NovedadesComponent implements OnInit  {
 
   urlNovedad: string;
   url: string;
+
+  displayed: boolean = false;
   constructor(
     public fb: FormBuilder,
     private publicService: PublicService,
+    private renderer2: Renderer2,
+    private el: ElementRef
   ) { }
 
   /*########### Form ###########*/
@@ -40,17 +44,41 @@ export class NovedadesComponent implements OnInit  {
     );
   }
 
+  ngAfterViewInit() {
 
+    let scriptEl = document.createElement('script');
+    scriptEl.src = 'https://platform.twitter.com/widgets.js'
+
+    this.renderer2.appendChild(this.el.nativeElement, scriptEl);
+
+  }
+
+  async delay(ms: number) {
+    await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
+}
 
   OnEnfermedadSubmit(){
     this.urlNovedad = this.enfermedadSeleccionadaFiled.value;
     this.data.sourceType = 'url';
     this.data.url = this.urlNovedad;
-    }
+    this.displayed = false;
+
+    this.delay(100).then(any=>{
+      console.log('holaaa');
+      this.displayed = true;
+
+      let scriptEl = document.createElement('script');
+      scriptEl.src = 'https://platform.twitter.com/widgets.js'
+
+      this.renderer2.appendChild(this.el.nativeElement, scriptEl);
+    });
+  }
 
   get enfermedadSeleccionadaFiled(){
     return this.EnfermedadForm.get('enfermedadSeleccionada'); // controls['examenSeleccionado'];
   }
+
+
 
 
 }
