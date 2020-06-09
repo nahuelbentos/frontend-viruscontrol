3122aecad4f28bda0660c92642b903b0fe645902
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-06-08 21:44:36 -0300

    quedo el chat SUPER PA BUENA

--- virus-control-uy/src/app/shared/services/medico.service.ts
+++ virus-control-uy/src/app/shared/services/medico.service.ts
@@ -3,63 +3,75 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { AutenticacionService } from './autenticacion.service';
 import { Ciudadano } from '@shared/model/Ciudadano';
+import { AngularFirestoreCollection } from '@angular/fire/firestore/public_api';
+import { Mensaje } from '@shared/model/chat/mensaje.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MedicoService {
   baseUrl = `${environment.url_backend}/medico`;
-  constructor(private http: HttpClient, private autenticacionService: AutenticacionService) { }
+
+  private mensajesCollection: AngularFirestoreCollection<Mensaje>;
+  
+  constructor(
+    private http: HttpClient,
+    private autenticacionService: AutenticacionService
+  ) {}
 
   public getVisitasPendientes() {
-    console.log('this.autenticacionService.user.email', this.autenticacionService.user);
-
+    console.log(
+      'this.autenticacionService.user.email',
+      this.autenticacionService.user
+    ); 
 
     // let headers = new Headers({ 'Content-Type': 'application/json' });
     // headers.append('Authorization', 'Bearer ');
     // let options = new RequestOptions({ headers: headers });
 
-    return this.http.get(`${environment.url_backend}/medico/visita_pendiente/all`);
+    return this.http.get(
+      `${environment.url_backend}/medico/visita_pendiente/all`
+    );
   }
+ 
 
-  public getEnfermedades(){
+  public getEnfermedades() {
     return this.http.get(`${this.baseUrl}/enfermedadesaprobadas`);
   }
   public confirmarVisitaPendiente(idVisita: number) {
-    return this.http.put(`${environment.url_backend}/medico/visita_pendiente/${idVisita}`,{});
+    return this.http.put(
+      `${environment.url_backend}/medico/visita_pendiente/${idVisita}`,
+      {}
+    );
   }
 
-  public getExamenes(id: number){
+  public getExamenes(id: number) {
     return this.http.get(`${this.baseUrl}/examenesenfermedades/${id}`);
   }
 
-  public getCiudadanosDeMedico(){ // TODO: Darle el id del medico cuando cambie el ws
+  public getCiudadanosDeMedico() {
+    // TODO: Darle el id del medico cuando cambie el ws
     return this.http.get(`${this.baseUrl}/ciudadanos`);
   }
 
-  public getProveedoresDeExamenesById(id: number){
+  public getProveedoresDeExamenesById(id: number) {
     console.log('getproveedoresexamenes');
     return this.http.get(`${this.baseUrl}/proveedoresexamen/${id}`);
   }
 
-  public getProveedoresDeExamenes(){
+  public getProveedoresDeExamenes() {
     console.log('getproveedoresexamenes');
     return this.http.get(`${this.baseUrl}/proveedoresexamen`);
   }
 
-  public getDepartamentos(){
+  public getDepartamentos() {
     return this.http.get(`${this.baseUrl}/departamentos`);
   }
 
-  public solicitarExamen(solicitarExamen: RequestSolicitarExamen){
+  public solicitarExamen(solicitarExamen: RequestSolicitarExamen) {
     console.log(solicitarExamen);
     return this.http.post(`${this.baseUrl}/nuevocaso`, solicitarExamen);
-
   }
-
-
-
-
 }
 
 
