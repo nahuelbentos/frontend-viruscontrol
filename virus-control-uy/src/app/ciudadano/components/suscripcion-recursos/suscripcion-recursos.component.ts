3bc311e723ea939467b3e575d35f1a22d47cd385
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-06-08 22:27:23 -0300

    fix suscripcion-recursos

--- virus-control-uy/src/app/ciudadano/components/suscripcion-recursos/suscripcion-recursos.component.ts
+++ virus-control-uy/src/app/ciudadano/components/suscripcion-recursos/suscripcion-recursos.component.ts
@@ -1,26 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { CiudadanoService, RequestSuscripcionRecursos } from '@shared/services/ciudadano.service';
+import {
+  CiudadanoService,
+  RequestSuscripcionRecursos,
+} from '@shared/services/ciudadano.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AutenticacionService } from '@shared/services/autenticacion.service';
 
 @Component({
   selector: 'app-suscripcion-recursos',
   templateUrl: './suscripcion-recursos.component.html',
-  styleUrls: ['./suscripcion-recursos.component.scss']
+  styleUrls: ['./suscripcion-recursos.component.scss'],
 })
 export class SuscripcionRecursosComponent implements OnInit {
-
   barrios: string[];
-  tipoRecurso: string[];
-  formaNotificacion: string[];
+  tipoRecursos: string[];
+  formaNotificaciones: string[];
   formSubmitted = false;
 
   constructor(
     public fb: FormBuilder,
     private ciudadanoService: CiudadanoService,
     private autenticacionService: AutenticacionService
-  ) { }
-
+  ) {}
 
   /*########### Form ###########*/
   SuscripcionForm = this.fb.group({
@@ -29,37 +30,31 @@ export class SuscripcionRecursosComponent implements OnInit {
     formaNotificacionSeleccionada: [null, Validators.required],
   });
 
-  onFormSubmit(){
+  onFormSubmit() {
     this.formSubmitted = true;
     if (!this.SuscripcionForm.valid) {
       return;
     }
 
-      const SuscripcionRecurso: RequestSuscripcionRecursos = {
-        idUsuario: this.autenticacionService.user.idUsuario,
-        barrio: this.barrioSeleccionadoFiled.value,
-        tipoRecurso: this.tipoRecursoSeleccionadoFiled.value,
-        formaNotificacion: this.formaNotificacionSeleccionadaFiled.value
-      };
+    const SuscripcionRecurso: RequestSuscripcionRecursos = {
+      idUsuario: this.autenticacionService.user.idUsuario,
+      barrio: this.barrioSeleccionadoFiled.value,
+      tipoRecurso: this.tipoRecursoSeleccionadoFiled.value,
+      formaNotificacion: this.formaNotificacionSeleccionadaFiled.value,
+    };
   }
 
+  ngOnInit(): void {}
 
-
-
-  ngOnInit(): void {
-  }
-
-  get barrioSeleccionadoFiled(){
+  get barrioSeleccionadoFiled() {
     return this.SuscripcionForm.get('barrioSeleccionado');
   }
 
-  get tipoRecursoSeleccionadoFiled(){
+  get tipoRecursoSeleccionadoFiled() {
     return this.SuscripcionForm.get('tipoRecursoSeleccionado');
   }
 
-  get formaNotificacionSeleccionadaFiled(){
+  get formaNotificacionSeleccionadaFiled() {
     return this.SuscripcionForm.get('formaNotificacionSeleccionada');
   }
-
-
 }
