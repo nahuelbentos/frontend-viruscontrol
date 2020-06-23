658be151e2a1fd8c891ff2b15c8826e2bf05df5e
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-06-22 23:45:09 -0300

    opcional del jhona

--- virus-control-uy/src/app/ciudadano/components/resultados-examenes/resultados-examenes.component.ts
+++ virus-control-uy/src/app/ciudadano/components/resultados-examenes/resultados-examenes.component.ts
@@ -4,26 +4,77 @@ import { MatTableDataSource } from '@angular/material/table';
 import { CiudadanoService } from '@shared/services/ciudadano.service';
 import { ResultadoExamen } from '@shared/model/ResultadoExamen';
 
-
 @Component({
   selector: 'app-resultados-examenes',
   templateUrl: './resultados-examenes.component.html',
-  styleUrls: ['./resultados-examenes.component.scss']
+  styleUrls: ['./resultados-examenes.component.scss'],
 })
 export class ResultadosExamenesComponent implements OnInit {
   private examenes: ResultadoExamen[];
   dataSource: MatTableDataSource<ResultadoExamen>;
-  displayedColumns: string[] = ['Examen', 'Resultado'];
-  constructor(private ciudadanoService: CiudadanoService) { }
+  displayedColumns: string[] = ['Examen', 'Resultado', 'reporte'];
+  constructor(private ciudadanoService: CiudadanoService) {}
 
   ngOnInit(): void {
-    this.getResultadosExamenes()
+    this.getResultadosExamenes();
   }
 
   getResultadosExamenes(): void {
-    this.ciudadanoService.getResultadosExamenes().subscribe((resultadoExamen: ResultadoExamen[]) =>{
-      console.log('res examen', resultadoExamen);
-      this.dataSource = new MatTableDataSource(resultadoExamen);
+    this.ciudadanoService
+      .getResultadosExamenes()
+      .subscribe((resultadoExamen: ResultadoExamen[]) => {
+        console.log('res examen', resultadoExamen);
+        this.dataSource = new MatTableDataSource(resultadoExamen);
+      });
+  }
+
+  descargarPDF(idCaso) {
+    this.ciudadanoService.getPDF(idCaso).subscribe((res: any) => {
+      console.log('res: ', res);
+      
+      var newBlob = new Blob([res], { type: 'application/pdf' });
+
+      // IE doesn't allow using a blob object directly as link href
+      // instead it is necessary to use msSaveOrOpenBlob
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(newBlob);
+        return;
+      }
+
+      // For other browsers:
+      // Create a link pointing to the ObjectURL containing the blob.
+      const data = window.URL.createObjectURL(newBlob);
+
+      var link = document.createElement('a');
+      link.href = data;
+      link.download = 'Je kar.pdf';
+      // this is necessary as link.click() does not work on the latest firefox
+      link.dispatchEvent(
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+          view: window,
+        })
+      );
+
+      setTimeout(function () {
+        // For Firefox it is necessary to delay revoking the ObjectURL
+        window.URL.revokeObjectURL(data);
+        link.remove();
+      }, 100);
+
+
+      // var file = new Blob([data], { type: 'application/pdf' });
+      // var fileURL = URL.createObjectURL(file);
+
+      // // if you want to open PDF in new tab
+      // window.open(fileURL);
+      // var a = document.createElement('a');
+      // a.href = fileURL;
+      // a.target = '_blank';
+      // a.download = 'bill.pdf';
+      // document.body.appendChild(a);
+      // a.click();
     });
   }
 }
--- virus-control-uy/src/app/shared/services/ciudadano.service.ts
+++ virus-control-uy/src/app/shared/services/ciudadano.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AutenticacionService } from './autenticacion.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CiudadanoService {
   baseUrl = `${environment.url_backend}/ciudadano`;
 
-  constructor(private http: HttpClient, private autenticacionService: AutenticacionService) {}
+  constructor(
+    private http: HttpClient,
+    private autenticacionService: AutenticacionService
+  ) {}
 
   public getResultadosExamenes() {
-    return this.http.get(`${this.baseUrl}/obtenerexamenes/${this.autenticacionService.user.idUsuario}`);
+    return this.http.get(
+      `${this.baseUrl}/obtenerexamenes/${this.autenticacionService.user.idUsuario}`
+    );
   }
 
   public getBarrios() {
@@ -20,12 +25,37 @@ export class CiudadanoService {
   }
 
   public getRecursosDeBarrio(barrio: string) {
-    return this.http.get(`${environment.url_backend}/recursos/disponibles/ciudad/-/barrio/${barrio}`);
+    return this.http.get(
+      `${environment.url_backend}/recursos/disponibles/ciudad/-/barrio/${barrio}`
+    );
   }
 
-  public postSuscripcionRecurso(suscripcionRecurso: RequestSuscripcionRecursos) {
+  public postSuscripcionRecurso(
+    suscripcionRecurso: RequestSuscripcionRecursos
+  ) {
     console.log(suscripcionRecurso);
-    return this.http.post(`${this.baseUrl}/suscribirseARecurso`, suscripcionRecurso);
+    return this.http.post(
+      `${this.baseUrl}/suscribirseARecurso`,
+      suscripcionRecurso
+    );
+  }
+
+  public getPDF(idCaso) {
+
+    const headers = new HttpHeaders();
+    // headers.set('Content-Type', 'application/pdf;');
+    headers.set('Aceppt', 'application/pdf;');
+    // headers.set('Content-Disposition', 'attachment;');
+    /*
+    , {
+      headers: headers,
+      responseType: 'blob' as 'json',
+    }
+    */
+    return this.http.get(`${this.baseUrl}/resultado/pdf/${idCaso}`, {
+      headers,
+      responseType: 'blob' as 'json',
+    });
   }
 }
 
